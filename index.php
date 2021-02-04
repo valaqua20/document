08487bbf4554dd73371408ee627ed36a6b31517f
valaqua20 <75556719+valaqua20@users.noreply.github.com>
2021-02-04 14:44:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Q'document</title>
     <link rel="stylesheet" href="csss.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
